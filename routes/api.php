be34caa06db49c82343f1ff99aa920017195b640
Edi Santoso <edi@kata.ai>
2019-04-18 10:48:32 +0700

    chore: refactor user controller

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('users', 'UserController');
+Route::resource('users', 'Api\UserController')->except(['create', 'edit']);
